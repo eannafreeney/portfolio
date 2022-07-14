f3a6b6b2f3e0cd0368a2960b345bba199f9335d3
Eanna <eannafreeney@gmail.com>
2022-07-14 19:09:01 +0200

    edited config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
+  siteMetadata: {},
   plugins: [
     `gatsby-plugin-mdx`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
-    // `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,24 +20,11 @@ module.exports = {
         path: `${__dirname}/src/content/projects`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-
     // {
-    //   resolve: `gatsby-plugin-manifest`,
+    //   resolve: `gatsby-source-filesystem`,
     //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-astronaut.png`, // This path is relative to the root of the site.
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
     //   },
     // },
   ],
